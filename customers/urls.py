f60b99ad8727ff95face64326a8a641bd05a53d0
jchan0524 <jchan@udel.edu>
2023-01-13 09:55:58 -0500

    Added registration

--- customers/urls.py
+++ customers/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.urls import path
 from django.contrib import admin
 from customers import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name ='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
     path(r'admin/', admin.site.urls),
     path('api/customers/', views.customers, name='customers'),
     path('api/customers/<int:id>/', views.Customer, name='Customer'),
+    path('api/register/', views.register, name = 'register')
+    
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,12 +1,15 @@
 from customers.models import customer
 from django.http import JsonResponse, Http404
-from customers.serializers import CustomerSerializer
-from rest_framework.decorators import api_view
+from customers.serializers import CustomerSerializer, UserSerializer
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def customers(request):
     if request.method == 'GET':
         
@@ -23,7 +26,10 @@ def customers(request):
 
 
 
+# Required to throw permission_classes for pages that require authentication
+
 @api_view(['GET', 'POST', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def Customer(request, id):
     try:
         data = customer.objects.get(pk=id)
@@ -44,3 +50,17 @@ def Customer(request, id):
     
     serializer = CustomerSerializer(data)
     return Response({'customer': serializer.data})
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        tokens = {
+            'refresh' : str(refresh),
+            'access' : str(refresh.access_token)
+
+        }
+        return Response(tokens, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
